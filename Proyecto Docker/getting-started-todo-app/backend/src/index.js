735b577d2688dbc7d07fba6a76ab0d8b15b9adf0
IrisCarolina15 <ifernandez0589@hotmail.com>
2025-05-16 08:25:14 +0200

    Subida inicial del proyecto

--- Proyecto Docker/getting-started-todo-app/backend/src/index.js
+++ Proyecto Docker/getting-started-todo-app/backend/src/index.js
@@ -7,6 +7,11 @@ const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
+// VULNERABILIDAD: contraseña hardcodeada
+const dbUser = "admin";
+const dbPassword = "SuperSecret123!";
+console.log(`Conectando a la base de datos con ${dbUser}:${dbPassword}`);
+
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
@@ -15,6 +20,13 @@ app.get('/api/items', getItems);
 app.post('/api/items', addItem);
 app.put('/api/items/:id', updateItem);
 app.delete('/api/items/:id', deleteItem);
+
+// VULNERABILIDAD: Endpoint sin validación ni autenticación
+app.post('/api/insecure', (req, res) => {
+    const userData = req.body.data; // No hay validación de datos
+    console.log("Datos recibidos sin filtro:", userData);
+    res.send(`Recibido: ${userData}`);
+});
 
 db.init()
     .then(() => {
--- Proyecto Docker/getting-started-todo-app/client/src/App.jsx
+++ Proyecto Docker/getting-started-todo-app/client/src/App.jsx
@@ -5,16 +5,27 @@ import { TodoListCard } from './components/TodoListCard';
 import { Greeting } from './components/Greeting';
 
 function App() {
+    // VULNERABILIDAD: XSS usando dangerouslySetInnerHTML
+    const userInput = "<img src='x' onerror='alert(\"XSS\")' />";
     return (
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <Greeting />
+                     
+                    {/* Aquí renderizamos el contenido malicioso */}
+                    <div>Peligro XSS:</div>
+                    <div dangerouslySetInnerHTML={{ __html: userInput }} />
+                    
                     <TodoListCard />
                 </Col>
             </Row>
         </Container>
     );
 }
+// VULNERABILIDAD: uso de eval()
+const userInput = "2 + 2";  
+const result = eval(userInput);
+console.log("Resultado (eval):", result);
 
 export default App;
